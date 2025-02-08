e2af4469ff6712c9fd00d6b44af326c2ece0c02d
Fidoi <elautoricoi@gmail.com>
2025-02-08 20:21:25 -0300

    Navbar movile fixed, cloudinary config

--- src/components/navbar/MenuNavbar.tsx
+++ src/components/navbar/MenuNavbar.tsx
@@ -7,48 +7,47 @@ import { DropDown } from './dropdown/DropDown';
 
 export const MenuNavbar = () => {
   return (
-    <nav>
-      <Navbar
-        isBordered
-        maxWidth='full'
-        height={'5rem'}
-        className='flex justify-between items-center fixed'
-      >
-        <NavbarContent justify='start'>
-          <ImageAudio />
-        </NavbarContent>
-        <NavbarContent className='sm:hidden' justify='center'>
-          <DropDown />
-        </NavbarContent>
-        <NavbarContent className='hidden sm:flex' justify='center'>
-          <NavbarItem>
-            <Link color='foreground' href='/'>
-              Inicio
-            </Link>
-          </NavbarItem>
-          <NavbarItem isActive>
-            <Link
-              color='primary'
-              aria-current='page'
-              href='/projects'
-              className='text-primary'
-            >
-              Proyectos
-            </Link>
-          </NavbarItem>
-          <NavbarItem isActive={false}>
-            <Link
-              color='foreground'
-              href='#'
-              className='pointer-events-none cursor-default opacity-50'
-            >
-              Integraciones
-            </Link>
-          </NavbarItem>
-        </NavbarContent>
-        <NavbarContent justify='end'></NavbarContent>
+    <Navbar
+      isBordered
+      maxWidth='full'
+      height={'5rem'}
+      className='flex justify-between items-center fixed'
+    >
+      <NavbarContent justify='start'>
+        <ImageAudio />
+      </NavbarContent>
+      <NavbarContent className='sm:hidden' justify='center'>
+        <DropDown />
+      </NavbarContent>
+      <NavbarContent className='hidden sm:flex' justify='center'>
+        <NavbarItem>
+          <Link color='foreground' href='/'>
+            Inicio
+          </Link>
+        </NavbarItem>
+        <NavbarItem isActive>
+          <Link
+            color='primary'
+            aria-current='page'
+            href='/projects'
+            className='text-primary'
+          >
+            Proyectos
+          </Link>
+        </NavbarItem>
+        <NavbarItem isActive={false}>
+          <Link
+            color='foreground'
+            href='#'
+            className='pointer-events-none cursor-default opacity-50'
+          >
+            Integraciones
+          </Link>
+        </NavbarItem>
+      </NavbarContent>
+      <NavbarContent justify='end'>
         <ThemeSwitcher />
-      </Navbar>
-    </nav>
+      </NavbarContent>
+    </Navbar>
   );
 };
